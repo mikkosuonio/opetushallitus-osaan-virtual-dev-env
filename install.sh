f74422679613af05c70ea66da29e4046962286dd
Mikko Suonio <mikko.suonio@solita.fi>
2015-09-23 14:26:53 +0300

    Korjaa asennusskripti

--- install.sh
+++ install.sh
@@ -8,18 +8,17 @@ add-apt-repository "deb http://fi.archive.ubuntu.com/ubuntu/ trusty multiverse"
 apt-add-repository ppa:ansible/ansible
 apt-get update
 
-apt-get install git
-apt-get install nodejs
+apt-get install -y git
+apt-get install -y nodejs
 ln -s /usr/bin/nodejs /usr/local/bin/node
-apt-get install npm
-apt-get install virtualbox
-apt-get install vagrant
-apt-get install software-properties-common
-apt-get install ansible
-apt-get install openjdk-7-jdk
+apt-get install -y npm
+apt-get install -y virtualbox
+apt-get install -y vagrant
+apt-get install -y software-properties-common
+apt-get install -y ansible
+apt-get install -y openjdk-7-jdk
 
 npm install -g bower grunt-cli
-chown vagrant.vagrant /home/vagrant/tmp
 
 pushd /usr/local/bin
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
